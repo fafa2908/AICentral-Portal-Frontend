340d2e51955d2c990cdde6e88048e75fa35edfa4
Teo Sheng Pu <teoshengpu22@gmail.com>
2022-02-16 17:17:38 +0800

    Personal Page completed - with user rights, admin actions and strict class restriction. Error message prompt for personal page and project dashboard for invalid input

--- Pimcore/src/Controller/AITeam/PersonalPageController.php
+++ Pimcore/src/Controller/AITeam/PersonalPageController.php
@@ -12,12 +12,15 @@ use Carbon\Carbon;
 
 class PersonalPageController extends FrontendController
 {
+    
     /**
      * @Template()
-     * @Route("/PersonalPage/{obj_ID}", name="PersonalPage_Root")
      * @Route("/AITeam/PersonalPage/{obj_ID}", name="AITeamPersonalPage")
      */    
     public function defaultAction(Request $request, string $obj_ID = "106"){
+        $currentUser = \Pimcore\Tool\Admin::getCurrentUser();
+        $isadmin = $currentUser ? $currentUser->isadmin(): false;
+        
         // GET Employee Object 
         $employeeObj = DataObject\Employee::getById($obj_ID);
         $employeeObj->setPosition(substr($employeeObj->getPosition(), strpos($employeeObj->getPosition(), "_") + 1));
@@ -25,22 +28,18 @@ class PersonalPageController extends FrontendController
         // GET Enrollment List for the Employee Object
         $enrollAllList = new DataObject\Enrollment\Listing();
         $enrollList = $enrollAllList->filterByEmpEnroll($employeeObj);
+        $enrollList ->setOrderKey('EnrolEndDate');
+        $enrollList ->setOrder('desc');
         
         // GET WorkExp List for the Employee Object
         $workExpAllList = new DataObject\WorkExperience\Listing();
         $workExpList = $workExpAllList->filterByEmpWork($employeeObj);
+        $workExpList -> setOrderKey('WorkEndDate');
+        $workExpList -> setOrder('desc');
         
         // GET SkillProficency List for the Employee Object
         $skillProfAllList = new DataObject\SkillProficiency\Listing();
         $skillProfList = $skillProfAllList->filterByEmpSkill($employeeObj);
-        
-        
-        $enrollList ->setOrderKey('EnrolEndDate');
-        $enrollList ->setOrder('desc');
-        
-        $workExpList -> setOrderKey('WorkEndDate');
-        $workExpList -> setOrder('desc');
-        
         $skillProfList -> setOrderKey('SkillLevel');
         $skillProfList -> setOrder('desc');
         
@@ -60,17 +59,23 @@ class PersonalPageController extends FrontendController
         
         $educationAllList = new DataObject\Education\Listing();
         $companyAllList = new DataObject\Company\Listing();
+        $skillAllList = new DataObject\Skills\Listing();
+        $skillTrainingAllList = new DataObject\SkillTraining\Listing();
         
         // $workExpAllList = new DataObject\WorkExperience\Listing();
         // $workExpList = $workExpAllList->filterByEmpWork($employeeObj);
         
         return $this->render('AITeam/PersonalPage.html.twig', [
-           "employee" => $employeeObj,
-           "enrollList" => $enrollList,
-           "workExpList" => $workExpList,
-           "skillProfList" => $skillProfList,
-           "educationAllList" => $educationAllList, 
-           "companyAllList" => $companyAllList, 
+             "isadmin" => $isadmin,
+            "currentUser" => $currentUser, 
+            "employee" => $employeeObj,
+            "enrollList" => $enrollList,
+            "workExpList" => $workExpList,
+            "skillProfList" => $skillProfList,
+            "educationAllList" => $educationAllList, 
+            "companyAllList" => $companyAllList, 
+            "skillAllList"=>$skillAllList,
+            "skillTrainingAllList"=>$skillTrainingAllList,
         ]);
     }
     
@@ -81,9 +86,14 @@ class PersonalPageController extends FrontendController
          return $this->render('default/default.html.twig');
      }
      
-        /**
+    private function alert($message="Sorry, An Error Occured"){
+        echo '<script language="javascript">';
+        echo 'alert("Error Message: '.$message.'")';
+        echo '</script>';
+    }
+    
+    /**
      * @Route("/AITeam/PersonalPage/forms/Company/{obj_ID}", name="CompanyFormPost")
-     * @Route("/PersonalPage/forms/Company/{obj_ID}", name="CompanyFormPost_Root")
      */
      public function CompanyForm(Request $request, string $obj_ID = "106"){
         
@@ -124,37 +134,24 @@ class PersonalPageController extends FrontendController
                 foreach($workExperienceList as $work){
                     array_push($utilisedCompany, $work->getCompanyWork()->geto_Id());
                 }
-                
                 if(! in_array($_POST['CompanyObjectList'],$utilisedCompany)){
                     $companyObj->delete();
                 }
                 else{
-                    echo "UNABLE TO BE DELETED <br> THIS OBJECT USED BY OTHER PEOPLE";
+                    $this->alert("Company Object Unable to be Deleted (Used by Other user)");
                 }
             }
-        
         return $this->redirectToRoute('AITeamPersonalPage',['obj_ID'=>$obj_ID],301);
-        
-
      }
      
        /**
      * @Route("/AITeam/PersonalPage/forms/WorkExperience/{obj_ID}", name="WorkExperienceFormPost")
-     * @Route("/PersonalPage/forms/WorkExperience/{obj_ID}", name="WorkExperienceFormPost_Root")
      */
      public function WorkExperienceForm(Request $request, string $obj_ID = "106"){
          $employeeObj = DataObject\Employee::getById($obj_ID);
          $companyObj = DataObject\Company::getById($_POST['Work_CompanyObjectList']);
          $workExperienceObj = DataObject\WorkExperience::getById($_POST['WorkObjectList']);
          
-        // echo $_POST['WorkObjectList'];
-        // echo $_POST['workTitle'];
-        // echo $_POST['workTypeList'];
-        // echo $_POST['workStartDate'];
-        // echo $_POST['workEndDate'];
-        // echo $_POST['WorkDesc'];
-        // echo $_POST['Work_CompanyObjectList'];
-        
          if ($_POST['buttonAction'] == "Save" and $_SERVER["REQUEST_METHOD"] == "POST"){
              
              $workObjName = $employeeObj->getName() . " - " . $companyObj->getCompany();
@@ -193,9 +190,8 @@ class PersonalPageController extends FrontendController
          return $this->redirectToRoute('AITeamPersonalPage',['obj_ID'=>$obj_ID],301);
      }
      
-          /**
+    /**
      * @Route("/AITeam/PersonalPage/forms/Education/{obj_ID}", name="EducationFormPost")
-     * @Route("/PersonalPage/forms/Education/{obj_ID}", name="EducationFormPost_Root")
      */    
      public function EducationForm(Request $request, string $obj_ID = "106"){
          $object = DataObject\Education::getById($_POST['EducationObjectList']); 
@@ -248,7 +244,6 @@ class PersonalPageController extends FrontendController
      
     /**
      * @Route("/AITeam/PersonalPage/forms/Enrollment/{obj_ID}", name="EnrollmentFormPost")
-     * @Route("/PersonalPage/forms/Enrollment/{obj_ID}", name="EnrollmentFormPost_Root")
      */
      public function EnrollmentForm(Request $request, string $obj_ID = "106"){
         // echo ("hello");
@@ -294,5 +289,151 @@ class PersonalPageController extends FrontendController
         return $this->redirectToRoute('AITeamPersonalPage',['obj_ID'=>$obj_ID],301);
         
     }
+    
+    /**
+     * @Route("/AITeam/PersonalPage/forms/SkillCategory/{obj_ID}", name="SkillCategoryFormPost")
+     */
+    public function SkillCategory(Request $request, string $obj_ID = "106"){
+        // echo ("hello");
+        // echo $_POST['inputSkillCategory'];
+        // echo $_POST['SkillCategoryObjectList']; 
+        $skillObj = DataObject\Skills::getById($_POST['SkillCategoryObjectList']); 
+        
+        if ($_POST['buttonAction'] == "Save" and $_SERVER["REQUEST_METHOD"] == "POST"){
+            if($_POST['SkillCategoryObjectList'] == "Create New Skill Catagory"){
+                // Create new Object
+                $newObject = new DataObject\Skills(); 
+                $newObject->setKey(\Pimcore\Model\Element\Service::getValidKey($_POST['inputSkillCategory'], 'object'));
+                $newObject->setParentId(77);            //root of folder
+                $newObject->setSkillCategory($_POST['inputSkillCategory']);
+                $newObject->setPublished(true);
+                $newObject->save();
+            }
+            else{
+                // Update new Object
+                $skillObj->setSkillCategory($_POST['inputSkillCategory']);
+                $skillObj->setPublished(true);
+                $skillObj->save();
+            }
+        }
+        else if ($_POST['buttonAction'] == "Delete" and $_SERVER["REQUEST_METHOD"] == "POST"){
+                $subSkillsList = new DataObject\SkillTraining\Listing();
+                $utilisedSkills = [];
+                foreach($subSkillsList as $skill){
+                    array_push($utilisedSkills, $skill->getSkillsCategoryDetails()->geto_Id());
+                }
+                
+                if(! in_array($_POST['SkillCategoryObjectList'],$utilisedSkills)){
+                    $skillObj->delete();
+                }
+                else{
+                    $this->alert("Skill Category Object unable to be deleted (Used by Other user)");
+                }
+            }
+        return $this->redirectToRoute('AITeamPersonalPage',['obj_ID'=>$obj_ID],301);
+    }
+    
+    /**
+     * @Route("/AITeam/PersonalPage/forms/SkillSubCategory/{obj_ID}", name="SkillSubCategoryFormPost")
+     */
+    public function SubSkillCategory(Request $request, string $obj_ID = "106"){
+        // echo ("hello");
+        echo json_encode($_POST);
+        // echo $_POST['subSkillCategoryObjectList']; 
+        
+        
+        $skillObj = DataObject\SkillTraining::getById($_POST['skillObjectList']); 
+        $skillCateObj = DataObject\Skills::getById($_POST['SkillCateObjectList']); 
+        
+        if ($_POST['buttonAction'] == "Save" and $_SERVER["REQUEST_METHOD"] == "POST"){
+            if($_POST['skillObjectList'] == "Create New Skill"){
+                // Create new Object
+                $newObject = new DataObject\SkillTraining(); 
+                $newObject->setKey(\Pimcore\Model\Element\Service::getValidKey($_POST['inputTrainingTitle'], 'object'));
+                $newObject->setParentId(303);            //root of folder
+                $newObject->setSkillSubCategory($_POST['inputSubCategory']);
+                $newObject->setTrainingTitle($_POST['inputTrainingTitle']);
+                $newObject->setExamName($_POST['inputExamName']);
+                $newObject->setSkillsCategoryDetails($skillCateObj);
+                $newObject->setPublished(true);
+                $newObject->save();
+            }
+            else{
+                // Update new Object
+                $skillObj->setSkillSubCategory($_POST['inputSubCategory']);
+                $skillObj->setTrainingTitle($_POST['inputTrainingTitle']);
+                $skillObj->setExamName($_POST['inputExamName']);
+                $skillObj->setSkillsCategoryDetails($skillCateObj);
+                $skillObj->setPublished(true);
+                $skillObj->save();
+            }
+        }
+        else if ($_POST['buttonAction'] == "Delete" and $_SERVER["REQUEST_METHOD"] == "POST"){
+                $profList = new DataObject\SkillProficiency\Listing();
+                $utilisedSkills = [];
+                foreach($profList as $prof){
+                    array_push($utilisedSkills, $prof->getSkillDetails()->geto_Id());
+                }
+                
+                if(! in_array($_POST['skillObjectList'],$utilisedSkills)){
+                    $skillObj->delete();
+                }
+                else{
+                    $this->alert("Skill Object unable to be deleted (Used by Other user)");
+                }
+                
+            }
+            
+        return $this->redirectToRoute('AITeamPersonalPage',['obj_ID'=>$obj_ID],301);
+    }
+    
+        /**
+     * @Route("/AITeam/PersonalPage/forms/SkillProficiency/{obj_ID}", name="SkillProficiencyFormPost")
+     */
+    public function SkillProficiency(Request $request, string $obj_ID = "106"){
+        // echo ("hello");
+        echo json_encode($_POST);
+        // echo $_POST['subSkillCategoryObjectList']; 
+        
+        $employeeObj = DataObject\Employee::getById($obj_ID);
+        $profObj = DataObject\SkillProficiency::getById($_POST['proficiencyObjectList']); 
+        $skillObj = DataObject\SkillTraining::getById($_POST['skillObject']); 
+        
+        $levelStatus = array_key_exists('proficiencyLevelList',$_POST) ? $_POST['proficiencyLevelList'] : "Level 0";
+        if ($_POST['buttonAction'] == "Save" and $_SERVER["REQUEST_METHOD"] == "POST"){
+            if($_POST['proficiencyObjectList'] == "Create New Proficiency"){
+                // Create new Object
+                $ObjName = $employeeObj->getName() . " - " . $skillObj->getTrainingTitle();
+                
+                $newObject = new DataObject\SkillProficiency(); 
+                $newObject->setKey(\Pimcore\Model\Element\Service::getValidKey($ObjName, 'object'));
+                $newObject->setParentId(74);            //root of folder
+                $newObject->setStatus($_POST['proficiencyStatusList']);
+                $newObject->setSkillLevel($levelStatus);
+                $newObject->setSkillYear(Carbon::parse($_POST['proficiencyDate']));
+                $newObject->setEmpSkill($employeeObj);
+                $newObject->setSkillDetails($skillObj);
+                $newObject->setPublished(true);
+                $newObject->save();
+            }
+            else{
+                // Update new Object
+                $profObj->setStatus($_POST['proficiencyStatusList']);
+                $profObj->setSkillLevel($levelStatus);
+                $profObj->setSkillYear(Carbon::parse($_POST['proficiencyDate']));
+                $profObj->setEmpSkill($employeeObj);
+                $profObj->setSkillDetails($skillObj);
+                $profObj->setPublished(true);
+                $profObj->save();
+            }
+        }
+        else if ($_POST['buttonAction'] == "Delete" and $_SERVER["REQUEST_METHOD"] == "POST"){
+                $profObj->delete();
+            }
+            
+        return $this->redirectToRoute('AITeamPersonalPage',['obj_ID'=>$obj_ID],301);
+    }
+    
+    
     
 }
